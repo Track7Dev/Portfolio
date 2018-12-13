c8cbcd2d4d5faa6c88aef59fab0a85a633c1e712
Brandon Fizer <brandon.m.fizer@gmail.com>
2018-12-13 13:46:43 -0500

    Menu Close

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
 const Menu = (props) => {
+const main = props.main;
   return (
     <div id='menu'  className={props.main.state.animations.headerMenu.open ? "App-Main-Menu-Open" : "App-Main-Menu-Close"}>
       <p onClick={
-          () =>  window.open(window.location.origin)
+          () =>  {main.closeMenu(); window.open(window.location.origin);}
         }>HOME</p>
       <p onClick={
-          () =>  window.location.replace('#about')
+          () =>  {main.closeMenu(); window.location.replace('#about');}
         }>ABOUT ME</p>
       <p onClick={
-          () =>  window.open('Resume-BrandonFizer-SE.pdf','_blank')
+          () =>  {main.closeMenu(); window.open('Resume-BrandonFizer-SE.pdf','_blank');}
         }>RESUME</p>
       <p onClick={
-        () => window.location.href = "mailto:Brandon.M.Fizer@gmail.com?subject=SE%20Inquiry&body="
+        () => {main.closeMenu(); window.location.href = "mailto:Brandon.M.Fizer@gmail.com?subject=SE%20Inquiry&body=";}
         }>CONTACT</p>
     </div>
   );
