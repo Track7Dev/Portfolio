e1889aea6a73782f0ff021b5fd8122d2e2929d6a
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2019-10-22 19:05:01 -0400

    added service worker

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+if(navigator.serviceWorker){
+  navigator.serviceWorker.register('sw.js')
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
